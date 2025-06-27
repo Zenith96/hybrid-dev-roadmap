17e6ac4b91965acaa14465432f522825757f71e4
Zenith96 <apurvjha9@gmail.com>
2025-06-27 12:34:54 +0530

    Day 2 done

--- Practice1.py
+++ Practice1.py
@@ -1,3 +1,15 @@
+def give_name(name):
+    print("Your name is: "+ name)
+def give_age(age):
+    print("Your age is:",age)
 name = input("What is your name? ")
-age = input("How old are you? ")
-print("Your name is " + name + ". Your age is " + age)
+give_name(name)
+while True:
+    age = int(input("How old are you? "))
+    if age > 0 and age < 120:
+        give_age(age)
+        break
+    else:
+        print("Try again!!")
+        print("New input: ")
+
